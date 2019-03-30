ab0230d3b771f0512cbccf6db0172a8924c55cd1
avidrutham <divyamathur89@gmail.com>
2019-03-30 22:29:33 +0530

    mlab db connected

--- server/app.js
+++ server/app.js
@@ -1,6 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.MLAB_URI, {
+    useNewUrlParser: true
+  })
+  .then(
+    () => {
+      console.log("Database is now connected");
+    },
+    err => {
+      console.log("Cannot connect to database + ", err);
+    }
+  );
 
 const app = express();
 app.use(
