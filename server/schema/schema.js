c919d27bab41d8cdaed1b93bb7738cb85f549b9d
avidrutham <divyamathur89@gmail.com>
2019-03-30 22:13:26 +0530

    add root queries for author and book lists

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -57,6 +57,18 @@ const RootQuery = new GraphQLObjectType({
         // code to get data from db
         return _.find(authors, { id: args.id });
       }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
+      }
     }
   }
 });
