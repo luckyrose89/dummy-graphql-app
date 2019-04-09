2fc593c51fc679c84245b9afcfa02579bd190e30
avidrutham <divyamathur89@gmail.com>
2019-04-09 23:30:19 +0530

    add event listeners to addBook

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -3,6 +3,28 @@ import { graphql } from "react-apollo";
 import { getAuthorsQuery } from "../queries/queries";
 
 class AddBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      genre: "",
+      authorId: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   displayAuthor() {
     var data = this.props.data;
     if (data.loading === true) {
@@ -19,21 +41,21 @@ class AddBook extends Component {
   }
   render() {
     return (
-      <form id="addBook">
+      <form id="addBook" onSubmit={this.handleSubmit}>
         <div className="field">
           <label>Book name</label>
-          <input type="text" />
+          <input type="text" name="name" onChange={this.handleChange} />
         </div>
         <div className="field">
           <label>Author</label>
-          <select>
+          <select name="authorId" onChange={this.handleChange}>
             <option>Select author</option>
             {this.displayAuthor()}
           </select>
         </div>
         <div className="field">
           <label>Genre</label>
-          <input type="text" />
+          <input type="text" name="genre" onChange={this.handleChange} />
         </div>
         <button>+</button>
       </form>
